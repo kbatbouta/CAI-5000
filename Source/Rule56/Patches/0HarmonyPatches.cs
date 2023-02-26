24112c9881d6a12812a0dc078e2114041ebc04d7
DullSaurcer <kbatbouta66@gmail.com>
2023-02-26 10:54:03 -0800

    ..

--- Source/Rule56/Patches/0HarmonyPatches.cs
+++ Source/Rule56/Patches/0HarmonyPatches.cs
@@ -1,3 +1,4 @@
+using System;
 using Verse;
 namespace CombatAI.Patches
 {
@@ -6,9 +7,21 @@ namespace CombatAI.Patches
         public static void Initialize()
         {
             // queue patches
-            LongEventHandler.QueueLongEvent(Finder.Harmony.PatchAll, "CombatAI.Preparing", false, null);
+            LongEventHandler.QueueLongEvent(PatchAll, "CombatAI.Preparing", false, OnError);
             // manual patches
             MainMenuDrawer_Patch.Patch();
+        }
+
+        private static void PatchAll()
+        {
+            Log.Message("ISMA: Applying patches");
+            Finder.Harmony.PatchAll();
+        }
+
+        private static void OnError(Exception exception)
+        {
+            Log.Error(exception.ToString());
+            Log.Error(exception.Message);
         }
     }
 }
